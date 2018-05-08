5bd4f70bf1528a195584937f7b375e0add0040f8
Hans <1337.jebus@gmail.com>
2018-05-08 22:19:06 +0200

    Dies ist der Anfang hoffentlich lange andauernder Python Studien.

---
+++ collect_devices.py
@@ -0,0 +1,16 @@
+class NETWORK_DEVICES:
+	device_counter = 0 #class variable that defines number of devices in class
+	def __init__(self, name, vendor, function, mgmt_ip):
+		self.name = name
+		self.vendor = vendor
+		self.function = function
+		self.mgmt_ip = mgmt_ip
+		NETWORK_DEVICES.device_counter += 1 #class variable gets altered
+	def remove(self, name):
+		print('You have removed{0}'.format(self.name)
+		NETWORK_DEVICES.device_counter -= 1 #class variable gets altered
+
+dev1 = NETWORK_DEVICES('core_hungen_1', 'cisco', 'switch', '10.255.7.130')
+dev2 = NETWORK_DEVICES('core_hungen_2', 'cisco', 'switch', '10.255.7.131')
+dev3 = NETWORK_DEVICES('fw_hungen', 'fortigate', 'firewall', '10.255.7.129')
+ 
---
+++ readme.txt
@@ -0,0 +1,4 @@
+08.05.2018
+Hier soll ein kleines Programm zum üben entstehen.
+Ziel ist es Netzwerk Geräte in einer DatenBank zu erfassen.
+Zum Anfang spiele ich ein wenig mit Klassen herum.
