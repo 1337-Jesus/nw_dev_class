6085c02ea5b83cf99f712df4527bc30bbc62b5f2
Patrick <patrick@Mint.BOX>
2018-05-09 16:31:20 +0200

    Zwei Subclasses wurden hinzugefuegt. Erste Schritte in Richtung Verebung unternommen.

--- collect_devices.py
+++ collect_devices.py
@@ -10,6 +10,17 @@ class NETWORK_DEVICES:
 		print('You have removed {0}'.format(self.name))
 		NETWORK_DEVICES.device_counter -= 1 #class variable gets altered
 
+
+class FIREWALL(NETWORK_DEVICES):
+	def __init__(self, name, vendor, function, mgmt_ip, vdom)
+		NETWORK_DEVICES.__init__(self, name, vendor, function, mgmt_ip)
+		self.vdom = vdom
+
+class SWITCH(NETWORK_DEVICES):
+	def __init__(self, name, vendor, function, mgmt_ip, ports)
+		NETWORK_DEVICES.__init__(self, name, vendor, function, mgmt_ip)
+		self.ports = ports
+
 dev1 = NETWORK_DEVICES('core_hungen_1', 'cisco', 'switch', '10.255.7.130')
 dev2 = NETWORK_DEVICES('core_hungen_2', 'cisco', 'switch', '10.255.7.131')
 dev3 = NETWORK_DEVICES('fw_hungen', 'fortigate', 'firewall', '10.255.7.129')
